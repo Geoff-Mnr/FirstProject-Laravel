0370fcb3f14115305e4721c6f09af85fc19455e8
Yakuz0r <geoffrey_meunier@live.be>
2023-10-03 18:50:18 +0200

    add authentification

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,13 +18,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-/*
-Route::get('/products',
-[\App\Http\Controllers\ProductsController::class, 'index']);*/
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resources([
+        'products' => \App\Http\Controllers\ProductsController::class,
+        'customers' => \App\Http\Controllers\CustomersController::class,
+    ]);
+});
 
-Route::resources([
-    'products' => \App\Http\Controllers\ProductsController::class,
-    'customers' => \App\Http\Controllers\CustomersController::class,
-]);
-
+require __DIR__.'/auth.php';
